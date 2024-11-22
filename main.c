47149b410bb0a2a9d68f60dda7e1f7c678dcd52a
kpierroti <85771363+kpierroti@users.noreply.github.com>
2024-11-22 13:49:19 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("example.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Failed to open file");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
